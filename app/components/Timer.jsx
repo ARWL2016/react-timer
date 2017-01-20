426ef09bf30b0730b2e34c5cb09458869816016a
Alistair Willis <alistairrwillis@gmail.com>
2017-01-20 14:07:43 +0900

    Add navigation

---
+++ app/components/Timer.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+
+var Timer = () => {
+    
+        return (
+            <div>
+                <h2>Timer Component</h2>
+            </div> 
+        );
+};
+
+module.exports = Timer; 
