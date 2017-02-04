a7a2e0eb4f399df95f5a053c841de14d4e0d5a6d
Alistair Willis <alistairrwillis@gmail.com>
2017-02-04 13:09:02 +0900

    finalise tests and add page title

--- app/tests/components/Countdown.test.jsx
+++ app/tests/components/Countdown.test.jsx
@@ -23,7 +23,7 @@ describe('Countdown', () => {
         expect(countdown.state.count).toBe(9);
         done(); 
       }, 1001);
-    });
+    }); //it
 
     it('should never count below zero', (done) => {
       var countdown = TestUtils.renderIntoDocument(<Countdown/>); 
@@ -33,9 +33,31 @@ describe('Countdown', () => {
         expect(countdown.state.count).toBe(0);
         done(); 
       }, 3000);
-    });
-  });
+    }); //it
 
+    it('should pause countdown on paused status', (done) => {
+      var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+      countdown.handleSetCountdown(3); 
+      countdown.handleStatusChange('paused'); 
 
+      setTimeout(() => {
+        expect(countdown.state.count).toBe(3);
+        expect(countdown.state.countdownStatus).toBe('paused');
+        done();  
+      }, 1001);
+    }); //it
+
+    it('should reset countdown on stopped status', (done) => {
+      var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+      countdown.handleSetCountdown(3); 
+      countdown.handleStatusChange('stopped'); 
+
+      setTimeout(() => {
+        expect(countdown.state.count).toBe(0);
+        expect(countdown.state.countdownStatus).toBe('stopped');
+        done();  
+      }, 1001);
+    }); //it
+  });//describe
 
 });//Countdown
